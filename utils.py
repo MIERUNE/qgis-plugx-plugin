764877d8835efa2f494ea01b3ac2a07b461cfa56
Kanahiro <mediterranean1769@gmail.com>
2023-07-13 11:09:28 +0900

    encure_ascii=False in write_json

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ from qgis.utils import iface
 
 def write_json(data: Union[list, dict], filepath: str):
     # Convert dictionary to JSON string
-    json_data = json.dumps(data)
+    json_data = json.dumps(data, ensure_ascii=False)
 
     # Write JSON string to file
     with open(filepath, "w") as outfile:
