4ea4c8992e2e140cb319d166f4f2e7f45fe98c73
Kanahiro <mediterranean1769@gmail.com>
2023-07-20 20:05:55 +0900

    wip

--- translator/raster/wms.py
+++ translator/raster/wms.py
@@ -1,93 +1,92 @@
 import os
 
-import processing
 from qgis.core import (
     QgsCoordinateReferenceSystem,
-    QgsCoordinateTransform,
     QgsProject,
     QgsRasterFileWriter,
     QgsRasterLayer,
     QgsRasterPipe,
     QgsRectangle,
+    Qgis,
+    QgsCoordinateTransform,
 )
 from qgis.utils import iface
 
-from utils import get_tempdir
+
+def _get_multiplier_by_unit_of(crs: QgsCoordinateReferenceSystem) -> float:
+    """return multiplier: crs unit -> inch"""
+    if crs.mapUnits() == Qgis.DistanceUnit.Meters:
+        multiplier_to_meter = 1.0
+    elif crs.mapUnits() == Qgis.DistanceUnit.Kilometers:
+        multiplier_to_meter = 1000.0
+    elif crs.mapUnits() == Qgis.DistanceUnit.Feet:
+        multiplier_to_meter = 0.3048
+    elif crs.mapUnits() == Qgis.DistanceUnit.NauticalMiles:
+        multiplier_to_meter = 1852.0
+    elif crs.mapUnits() == Qgis.DistanceUnit.Yards:
+        multiplier_to_meter = 0.9144
+    elif crs.mapUnits() == Qgis.DistanceUnit.Miles:
+        multiplier_to_meter = 1609.344
+    elif crs.mapUnits() == Qgis.DistanceUnit.Degrees:
+        multiplier_to_meter = 111319.49079327358  # at equator
+    elif crs.mapUnits() == Qgis.DistanceUnit.Centimeters:
+        multiplier_to_meter = 0.01
+    elif crs.mapUnits() == Qgis.DistanceUnit.Millimeters:
+        multiplier_to_meter = 0.001
+    elif crs.mapUnits() == Qgis.DistanceUnit.Inches:
+        multiplier_to_meter = 0.0254
+    else:
+        multiplier_to_meter = 1.0  # fallback
+
+    multiplier_to_inch = multiplier_to_meter / 0.0254
+    return multiplier_to_inch
 
 
 def process_wms(layer: QgsRasterLayer, extent: QgsRectangle, idx: int, output_dir: str):
     """process wms, xyz..."""
-    # Convert Bbox to EPSG:3857
+
+    # extent is in Project crs, transform to layer crs
     transform = QgsCoordinateTransform(
         QgsProject.instance().crs(),
-        QgsCoordinateReferenceSystem("EPSG:3857"),
+        layer.crs(),
         QgsProject.instance(),
     )
-    extent_3857 = transform.transformBoundingBox(extent)
-    extent_to_clip = QgsRectangle(
-        extent_3857.xMinimum(),
-        extent_3857.yMinimum(),
-        extent_3857.xMaximum(),
-        extent_3857.yMaximum(),
-    )
+    extent_in_layer_crs = transform.transformBoundingBox(extent)
 
-    # Calculate extent in EPSG:3857
-    extent_width = extent_to_clip.xMaximum() - extent_to_clip.xMinimum()
-    extent_height = extent_to_clip.yMaximum() - extent_to_clip.yMinimum()
+    # set project crs for layer
+    _layer = layer.clone()
+    _layer.setCrs(QgsProject.instance().crs())
+
+    extent_width = extent.xMaximum() - extent.xMinimum()
+    extent_height = extent.yMaximum() - extent.yMinimum()
 
     # Calculate image size in pixels
     dpi = iface.mapCanvas().mapSettings().outputDpi()  # dots / inch
-    inch_to_meter = 0.0254  # 1 inch = 0.0254 meter
-    # length_in_dots = length_in_meter / (dpm = dpi/inch_to_meter)
-    image_width = int(extent_width / dpi / inch_to_meter)
-    image_height = int(extent_height / dpi / inch_to_meter)
+    inch_to_crs_unit = _get_multiplier_by_unit_of(QgsProject.instance().crs())
+    # length_in_dots = length_in_crs_unit / (dpm = dpi/inch_to_crs_unit)
+    image_width = int(extent_width / dpi * inch_to_crs_unit)
+    image_height = int(extent_height / dpi * inch_to_crs_unit)
 
-    clipped_tiff_path = os.path.join(
-        get_tempdir(output_dir), f"{layer.name()}_clipped.tif"
+    # export png in project crs
+    file_writer = QgsRasterFileWriter(
+        os.path.join(output_dir, f"layer_{idx}.png"),
     )
-
-    # Save TIFF file in EPSG:3857
-    file_writer = QgsRasterFileWriter(clipped_tiff_path)
+    file_writer.setOutputFormat("PNG")
     pipe = QgsRasterPipe()
     pipe.set(layer.dataProvider().clone())
     file_writer.writeRaster(
         pipe,
         image_width,
         image_height,
-        extent_to_clip,
-        QgsCoordinateReferenceSystem("EPSG:3857"),
+        extent_in_layer_crs,
+        QgsProject.instance().crs(),
     )
 
-    # Convert to Project CRS
-    warped = processing.run(
-        "gdal:warpreproject",
-        {
-            "INPUT": clipped_tiff_path,
-            "SOURCE_CRS": QgsCoordinateReferenceSystem("EPSG:3857"),
-            "TARGET_CRS": QgsProject.instance().crs(),
-            "OUTPUT": os.path.join(
-                get_tempdir(output_dir), f"{layer.name()}_warped.tif"
-            ),
-        },
-    )["OUTPUT"]
-
-    # Create clip PNG file in Project CRS
-    clip_extent = f"{extent.xMinimum()}, \
-                    {extent.xMaximum()}, \
-                    {extent.yMinimum()}, \
-                    {extent.yMaximum()}  \
-                    [{QgsProject.instance().crs().authid()}]"
-
-    processing.run(
-        "gdal:cliprasterbyextent",
-        {
-            "INPUT": warped,
-            "PROJWIN": clip_extent,
-            "OVERCRS": False,
-            "NODATA": None,
-            "OPTIONS": "",
-            "DATA_TYPE": 0,
-            "EXTRA": "",
-            "OUTPUT": os.path.join(output_dir, f"layer_{idx}.png"),
-        },
-    )
+    # write world file
+    with open(os.path.join(output_dir, f"layer_{idx}.pgw"), "w") as f:
+        f.write(f"{extent_width / image_width}\n")
+        f.write("0\n")
+        f.write("0\n")
+        f.write(f"{-extent_height / image_height}\n")
+        f.write(f"{extent.xMinimum()}\n")
+        f.write(f"{extent.yMaximum()}\n")
