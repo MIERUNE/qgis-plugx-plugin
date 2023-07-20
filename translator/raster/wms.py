7a2322d2033042476e0f9cbc424c9cf867c5128f
Kanahiro <mediterranean1769@gmail.com>
2023-07-20 21:20:11 +0900

    fix: calc output image size from mapcanvas

--- translator/raster/wms.py
+++ translator/raster/wms.py
@@ -1,48 +1,17 @@
 import os
 
 from qgis.core import (
-    QgsCoordinateReferenceSystem,
     QgsProject,
     QgsRasterFileWriter,
     QgsRasterLayer,
     QgsRasterPipe,
     QgsRectangle,
-    Qgis,
     QgsCoordinateTransform,
 )
 from qgis.utils import iface
 import processing
 
 from utils import get_tempdir
-
-
-def _get_multiplier_by_unit_of(crs: QgsCoordinateReferenceSystem) -> float:
-    """return multiplier: crs unit -> inch"""
-    if crs.mapUnits() == Qgis.DistanceUnit.Meters:
-        multiplier_to_meter = 1.0
-    elif crs.mapUnits() == Qgis.DistanceUnit.Kilometers:
-        multiplier_to_meter = 1000.0
-    elif crs.mapUnits() == Qgis.DistanceUnit.Feet:
-        multiplier_to_meter = 0.3048
-    elif crs.mapUnits() == Qgis.DistanceUnit.NauticalMiles:
-        multiplier_to_meter = 1852.0
-    elif crs.mapUnits() == Qgis.DistanceUnit.Yards:
-        multiplier_to_meter = 0.9144
-    elif crs.mapUnits() == Qgis.DistanceUnit.Miles:
-        multiplier_to_meter = 1609.344
-    elif crs.mapUnits() == Qgis.DistanceUnit.Degrees:
-        multiplier_to_meter = 111319.49079327358  # at equator
-    elif crs.mapUnits() == Qgis.DistanceUnit.Centimeters:
-        multiplier_to_meter = 0.01
-    elif crs.mapUnits() == Qgis.DistanceUnit.Millimeters:
-        multiplier_to_meter = 0.001
-    elif crs.mapUnits() == Qgis.DistanceUnit.Inches:
-        multiplier_to_meter = 0.0254
-    else:
-        multiplier_to_meter = 1.0  # fallback
-
-    multiplier_to_inch = multiplier_to_meter / 0.0254
-    return multiplier_to_inch
 
 
 def process_wms(layer: QgsRasterLayer, extent: QgsRectangle, idx: int, output_dir: str):
@@ -56,15 +25,14 @@ def process_wms(layer: QgsRasterLayer, extent: QgsRectangle, idx: int, output_di
     )
     extent_in_layer_crs = transform.transformBoundingBox(extent)
 
-    # Calculate image size in pixels
-    dpi = iface.mapCanvas().mapSettings().outputDpi()  # dots / inch
-    inch_to_crs_unit = _get_multiplier_by_unit_of(QgsProject.instance().crs())
     # xy length in project-crs unit
     extent_width = extent.xMaximum() - extent.xMinimum()
     extent_height = extent.yMaximum() - extent.yMinimum()
-    # length_in_dots = length_in_crs_unit / (dpm = dpi/inch_to_crs_unit)
-    image_width = int(extent_width / dpi * inch_to_crs_unit)
-    image_height = int(extent_height / dpi * inch_to_crs_unit)
+
+    # calculate image size: same to map canvas
+    units_per_pixel = iface.mapCanvas().mapUnitsPerPixel()
+    image_width = int(extent_width / units_per_pixel)
+    image_height = int(extent_height / units_per_pixel)
 
     # export layer as tiff
     tiff_path = os.path.join(get_tempdir(output_dir), f"layer_{idx}.tiff")
