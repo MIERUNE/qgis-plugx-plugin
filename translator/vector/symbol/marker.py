fc6932c2adafc3e950c955e8cbe1c046c8a0782e
bordoray <lay.ishizuka@gmail.com>
2023-07-21 12:36:07 +0900

    Fix enum, add offset/ rotation to svg and

--- translator/vector/symbol/marker.py
+++ translator/vector/symbol/marker.py
@@ -1,4 +1,4 @@
-from qgis.core import QgsSymbolLayer, QgsMarkerSymbolLayer
+from qgis.core import QgsSymbolLayer, QgsSimpleMarkerSymbolLayer
 
 
 from utils import convert_to_point
@@ -7,29 +7,29 @@ from translator.vector.symbol.utils import get_asset_name, to_rgba
 
 def _get_markershape_from(symbol_layer: QgsSymbolLayer) -> dict:
     return {
-        QgsMarkerSymbolLayer.Shape.Square: "square",
-        QgsMarkerSymbolLayer.Shape.Diamond: "diamond",
-        QgsMarkerSymbolLayer.Shape.Pentagon: "pentagon",
-        QgsMarkerSymbolLayer.Shape.Hexagon: "hexagon",
-        QgsMarkerSymbolLayer.Shape.Triangle: "triangle",
-        QgsMarkerSymbolLayer.Shape.EquilateralTriangle: "equilateraltriangle",
-        QgsMarkerSymbolLayer.Shape.Star: "star",
-        QgsMarkerSymbolLayer.Shape.Arrow: "arrow",
-        QgsMarkerSymbolLayer.Shape.Circle: "circle",
-        QgsMarkerSymbolLayer.Shape.Cross: "cross",
-        QgsMarkerSymbolLayer.Shape.CrossFill: "crossfill",
-        QgsMarkerSymbolLayer.Shape.Cross2: "cross2",
-        QgsMarkerSymbolLayer.Shape.Line: "line",
-        QgsMarkerSymbolLayer.Shape.ArrowHead: "arrowhead",
-        QgsMarkerSymbolLayer.Shape.ArrowHeadFilled: "arrowheadfilled",
-        QgsMarkerSymbolLayer.Shape.SemiCircle: "semicircle",
-        QgsMarkerSymbolLayer.Shape.ThirdCircle: "thirdcircle",
-        QgsMarkerSymbolLayer.Shape.QuarterCircle: "quartercircle",
-        QgsMarkerSymbolLayer.Shape.QuarterSquare: "quartersquare",
-        QgsMarkerSymbolLayer.Shape.HalfSquare: "halfsquare",
-        QgsMarkerSymbolLayer.Shape.DiagonalHalfSquare: "diagonalhalfsquare",
-        QgsMarkerSymbolLayer.Shape.RightHalfTriangle: "righthalftriangle",
-        QgsMarkerSymbolLayer.Shape.LeftHalfTriangle: "lefthalftriangle",
+        QgsSimpleMarkerSymbolLayer.Shape.Square: "square",
+        QgsSimpleMarkerSymbolLayer.Shape.Diamond: "diamond",
+        QgsSimpleMarkerSymbolLayer.Shape.Pentagon: "pentagon",
+        QgsSimpleMarkerSymbolLayer.Shape.Hexagon: "hexagon",
+        QgsSimpleMarkerSymbolLayer.Shape.Triangle: "triangle",
+        QgsSimpleMarkerSymbolLayer.Shape.EquilateralTriangle: "equilateraltriangle",
+        QgsSimpleMarkerSymbolLayer.Shape.Star: "star",
+        QgsSimpleMarkerSymbolLayer.Shape.Arrow: "arrow",
+        QgsSimpleMarkerSymbolLayer.Shape.Circle: "circle",
+        QgsSimpleMarkerSymbolLayer.Shape.Cross: "cross",
+        QgsSimpleMarkerSymbolLayer.Shape.CrossFill: "crossfill",
+        QgsSimpleMarkerSymbolLayer.Shape.Cross2: "cross2",
+        QgsSimpleMarkerSymbolLayer.Shape.Line: "line",
+        QgsSimpleMarkerSymbolLayer.Shape.ArrowHead: "arrowhead",
+        QgsSimpleMarkerSymbolLayer.Shape.ArrowHeadFilled: "arrowheadfilled",
+        QgsSimpleMarkerSymbolLayer.Shape.SemiCircle: "semicircle",
+        QgsSimpleMarkerSymbolLayer.Shape.ThirdCircle: "thirdcircle",
+        QgsSimpleMarkerSymbolLayer.Shape.QuarterCircle: "quartercircle",
+        QgsSimpleMarkerSymbolLayer.Shape.QuarterSquare: "quartersquare",
+        QgsSimpleMarkerSymbolLayer.Shape.HalfSquare: "halfsquare",
+        QgsSimpleMarkerSymbolLayer.Shape.DiagonalHalfSquare: "diagonalhalfsquare",
+        QgsSimpleMarkerSymbolLayer.Shape.RightHalfTriangle: "righthalftriangle",
+        QgsSimpleMarkerSymbolLayer.Shape.LeftHalfTriangle: "lefthalftriangle",
     }.get(
         symbol_layer.shape(), "circle"  # fallback
     )
@@ -41,6 +41,11 @@ def get_point_symbol_data(symbol_layer: QgsSymbolLayer) -> dict:
             "size": convert_to_point(symbol_layer.size(), symbol_layer.sizeUnit()),
             "type": "raster",
             "asset_path": "assets/raster/" + get_asset_name(symbol_layer),
+            "offset": [
+                convert_to_point(symbol_layer.offset().x(), symbol_layer.offsetUnit()),
+                convert_to_point(symbol_layer.offset().y(), symbol_layer.offsetUnit()),
+            ],
+            "rotation": symbol_layer.angle(),
             "level": symbol_layer.renderingPass(),  # renderingPass means symbolLevels
             # https://github.com/qgis/QGIS/blob/65d40ee0ce59e761ee2de366ca9a963f35adfcfd/src/core/vector/qgsvectorlayerrenderer.cpp#L702
         }
@@ -55,6 +60,11 @@ def get_point_symbol_data(symbol_layer: QgsSymbolLayer) -> dict:
             ),
             "type": "svg",
             "asset_path": "assets/svg/" + get_asset_name(symbol_layer),
+            "offset": [
+                convert_to_point(symbol_layer.offset().x(), symbol_layer.offsetUnit()),
+                convert_to_point(symbol_layer.offset().y(), symbol_layer.offsetUnit()),
+            ],
+            "rotation": symbol_layer.angle(),
             "level": symbol_layer.renderingPass(),
         }
 
@@ -72,6 +82,7 @@ def get_point_symbol_data(symbol_layer: QgsSymbolLayer) -> dict:
                 convert_to_point(symbol_layer.offset().x(), symbol_layer.offsetUnit()),
                 convert_to_point(symbol_layer.offset().y(), symbol_layer.offsetUnit()),
             ],
+            "rotation": symbol_layer.angle(),
             "level": symbol_layer.renderingPass(),
         }
 
