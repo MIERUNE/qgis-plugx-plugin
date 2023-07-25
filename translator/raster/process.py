406739862946eb9ea006e983cb947303b4718b2a
Kanahiro <mediterranean1769@gmail.com>
2023-07-25 19:08:06 +0900

    integrate modules and use same logic for exporting

--- translator/raster/process.py
+++ translator/raster/process.py
@@ -3,21 +3,48 @@ import os
 from qgis.core import (
     QgsRasterLayer,
     QgsRectangle,
+    QgsProject,
+    QgsMapSettings,
+    QgsMapRendererParallelJob,
 )
+from qgis.utils import iface
+from PyQt5.QtCore import QSize
 
 from utils import write_json
-from .wms import process_wms
-from .file import process_file
 
 
 def process_raster(
     layer: QgsRasterLayer, extent: QgsRectangle, idx: int, output_dir: str
 ):
-    print(layer.name(), layer.providerType())
-    if layer.providerType() == "wms":
-        process_wms(layer, extent, idx, output_dir)
-    else:
-        process_file(layer, extent, idx, output_dir)
+    # xy length in project-crs unit
+    extent_width = extent.xMaximum() - extent.xMinimum()
+    extent_height = extent.yMaximum() - extent.yMinimum()
+
+    # calculate image size: same to map canvas
+    units_per_pixel = iface.mapCanvas().mapUnitsPerPixel()
+    image_width = int(extent_width / units_per_pixel)
+    image_height = int(extent_height / units_per_pixel)
+
+    # Set map to export image
+    settings = QgsMapSettings()
+    settings.setDestinationCrs(QgsProject.instance().crs())
+    settings.setLayers([layer])
+    settings.setExtent(extent)
+    settings.setOutputSize(QSize(image_width, image_height))
+
+    # Render the map
+    render = QgsMapRendererParallelJob(settings)
+    render.start()
+    render.waitForFinished()
+
+    # export rendered image as png
+    image = render.renderedImage()
+    image.save(os.path.join(output_dir, f"layer_{idx}.png"), "png")
+
+    # make world file for check
+    with open(os.path.join(output_dir, f"layer_{idx}.pgw"), "w") as f:
+        f.write(f"{units_per_pixel}\n0.0\n0.0\n-{units_per_pixel}\n")
+        f.write(f"{extent.xMinimum()}\n{extent.yMaximum()}\n")
 
     # json
     raster_info = {
