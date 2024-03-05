bbc23ffbf3a287baa7876595315188989e7a6d58
bordoray <lay.ishizuka@gmail.com>
2024-03-05 12:07:07 +0900

    Hairline conversion with QgsRenderContext

--- translator/vector/symbol/utils.py
+++ translator/vector/symbol/utils.py
@@ -4,6 +4,7 @@ from qgis.core import QgsSymbolLayer, QgsUnitTypes
 from PyQt5.QtGui import QColor
 from plugx_utils import convert_to_point
 
+
 def get_asset_dir(output_dir: str):
     return os.path.join(output_dir, "assets")
 
@@ -20,14 +21,16 @@ def to_rgba(color: QColor):
 
     return f"{color.name()}{alpha}"
 
+
 def get_stroke_width_pt(defined_width: float, unit: QgsUnitTypes.RenderUnit) -> float:
-    """ get line width in points, considering hairline or not 
+    """get line width in points, considering hairline or not
     symbol layer stroke width is set as 0 when hairline
     to be render as 0.5px (0.375pt) as defined in QGIS code
     https://github.com/qgis/QGIS/blob/e7f5b6f286b6cbaeaad5e73d69fad1448018c7e0/
     tests/src/python/test_qgssymbollayer_createsld.py#L234
     """
     if defined_width == 0:
-        return 0.375 # pt (hairline size: 0.5px)
+        hairline_px_size = 0.5
+        return convert_to_point(hairline_px_size, QgsUnitTypes.RenderPixels)
     else:
         return convert_to_point(defined_width, unit)
